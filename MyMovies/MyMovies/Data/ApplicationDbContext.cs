144d1aae2087e3b485c3499efb777b82b59e0462
Michał Janas <michal.janas1@outlook.com>
2022-11-15 00:37:25 +0100

    dodanie dbcontextu + connection stringa

---
+++ MyMovies/MyMovies/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyMovies.Models;
+
+namespace MyMovies.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
