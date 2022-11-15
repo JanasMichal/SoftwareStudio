d25c1211a23d2f6a457996166c7654d66819ded9
Michał Janas <michal.janas1@outlook.com>
2022-11-15 20:01:42 +0100

    dodanie brakujących pól

--- MyMovies/MyMovies/Models/Movie.cs
+++ MyMovies/MyMovies/Models/Movie.cs
@@ -7,8 +7,10 @@ namespace MyMovies.Models
         [Key]
         public int Id { get; set; }
         public string Title { get; set; }
+        public string? Director { get; set; }
         public int? YearOfProduction { get; set; }
         public string? Type { get; set; }
+        public double Rate { get; set; }
         public int? ExternalId { get; set; }
     }
 }
